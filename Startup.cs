bf5d2cb90f2aab7087ea5cfaaa4aa0add4431cbd
Nguyễn Đức Toàn <toanndse02449@fpt.edu.vn>
2018-04-19 18:05:51 +0700

    test

--- Startup.cs
+++ Startup.cs
@@ -46,7 +46,7 @@ namespace SchoolDashboard
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
-                configuration.RootPath = "ClientApp/dist";
+                configuration.RootPath = "dist";
             });
 
             services.AddSwaggerGen(c =>
@@ -110,7 +110,7 @@ namespace SchoolDashboard
                 // To learn more about options for serving an Angular SPA from ASP.NET Core,
                 // see https://go.microsoft.com/fwlink/?linkid=864501
 
-                spa.Options.SourcePath = "ClientApp";
+                spa.Options.SourcePath = "/";
 
                 if (env.IsDevelopment())
                 {

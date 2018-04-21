39e1168fb46c4d53094ce5a0184ae6a47e8545ea
Nguyễn Đức Toàn <toanndse02449@fpt.edu.vn>
2018-04-22 00:36:34 +0700

    update

---
+++ ClientApp/app/auth/_models/user.ts
@@ -0,0 +1,6 @@
+export class User {
+    id: number;
+    phone: string;
+    password: string;
+    fullName: string;
+}
--- ClientApp/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-
-export const environment = {
-    production: true,
-    baseUrl: null, //Change this to the address of your backend API if different from frontend address
-    loginUrl: "/Login"
-};
